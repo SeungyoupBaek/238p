8fd14dcd885512c1743e5f8770a575ecea07dc04
SeungyoupBaek <bsy8220@naver.com>
2014-01-08 01:07:29 +0900

    Initial Commit

---
+++ 238pTests/_38pTests.m
@@ -0,0 +1,34 @@
+//
+//  _38pTests.m
+//  238pTests
+//
+//  Created by Youp on 2014. 1. 8..
+//  Copyright (c) 2014년 admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _38pTests : XCTestCase
+
+@end
+
+@implementation _38pTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
